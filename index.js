6f657b6072677d540ff3f02dcb51d2132fe9ee5a
Princess <dpadrianox@gmail.com>
2025-06-02 15:02:43 -0400

    footer details

---
+++ index.js
@@ -0,0 +1,8 @@
+const API_KEY = 'e4af19b0';
+const BASE_URL = 'https://www.omdbapi.com/';
+
+async function fetchMovie(title) {
+  const response = await fetch(`${BASE_URL}?t=${title}&apikey=${API_KEY}`);
+  const data = await response.json();
+  return data;
+}
