4bc0b5709295feffc72b91c2e8d4be116f4011ea
Princess <dpadrianox@gmail.com>
2025-06-03 13:53:18 -0400

    menu backdrop transition

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const API_KEY = 'e4af19b0';
-const BASE_URL = 'https://www.omdbapi.com/';
+// const API_KEY = 'e4af19b0';
+// const BASE_URL = 'https://www.omdbapi.com/';
 
-async function fetchMovie(title) {
-  const response = await fetch(`${BASE_URL}?t=${title}&apikey=${API_KEY}`);
-  const data = await response.json();
-  return data;
+// async function fetchMovie(title) {
+//   const response = await fetch(`${BASE_URL}?t=${title}&apikey=${API_KEY}`);
+//   const data = await response.json();
+//   return data;
+// }
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+function closeMenu() {
+    document.body.classList.remove('menu--open')
 }
